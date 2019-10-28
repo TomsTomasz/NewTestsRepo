2e2929da15e149c35b7e2e1638500e0317ce68f5
Tomasz Ostrowski <tomaszostrowski365@gmail.com>
2019-10-28 18:52:06 +0100

    add random number picker

---
+++ RandomNumberPicker/ConsoleApp3/Metody.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Metody
+    {
+        public void Run()
+        {
+            Console.WriteLine("Ognia");
+            Console.WriteLine("10");
+            Console.WriteLine("9");
+            Console.WriteLine("8");
+            Console.WriteLine("7");
+            Console.WriteLine("6");
+            Console.WriteLine("5");
+            Console.WriteLine("4");
+            Console.WriteLine("3");
+            Console.WriteLine("2");
+            Console.WriteLine("1");
+            Console.WriteLine("Twoj wynik to: ");
+
+
+        }
+    }
+}
---
+++ RandomNumberPicker/ConsoleApp3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[] testArray = { "4", "10", "15", "18", "25", "35","42", "43", "138", "107","111", "122", "123", "124", "127","132" };
+
+            List<string> myList = new List<string>();
+
+            Random random = new Random();
+            int index = random.Next(testArray.Length);
+
+            Metody methods = new Metody();
+
+            methods.Run();
+            Console.WriteLine($"Liczba pod ktora kryje sie cos co bede robil to: {testArray[index]}");
+
+            Console.ReadKey();
+
+        }
+
+
+        
+    }
+}
