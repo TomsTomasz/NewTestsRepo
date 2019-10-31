a22e2255e6e2cfe91d9a4d316666ad38fe0b5187
Tomasz Ostrowski <tomaszostrowski365@gmail.com>
2019-10-31 18:21:23 +0100

    add folder and browse tab

---
+++ ASCII ART/ASCII ART/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ASCII_ART
+{
+    public partial class Form1 : Form3
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Browse_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog result = new OpenFileDialog();
+            result.Filter = "Image files (*.jpg, *.jpeg, *.jpe, *.jfif, *.png) | *.jpg; *.jpeg; *.jpe; *.jfif; *.png";
+            if (result.ShowDialog() == DialogResult.OK)
+            {
+                textBox1.Text = result.FileName;
+            }
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form2 forma = new Form2();
+            this.Close();
+            forma.ShowDialog();
+        }
+
+        
+    }
+}
